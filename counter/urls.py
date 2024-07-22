e5e5e3775fdb3bbe1736389c1b6cb1a9f404481e
Prem Kumar <premkumar@Prems-MacBook-Air.local>
2024-07-22 18:17:38 -0500

    url

--- counter/urls.py
+++ counter/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import JarCountViewSet, update_jar_count
 
 router = DefaultRouter()
-router.register(r'jarcounts', JarCountViewSet)
+router.register(r'boxercounts', JarCountViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
