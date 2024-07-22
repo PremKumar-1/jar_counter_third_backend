3fd9adc220bc6e565bec99d6c4e94a9bd843f7a3
Prem Kumar <premkumar@Prems-MacBook-Air.local>
2024-07-22 15:28:06 -0500

    removed inventory

--- counter/serializers.py
+++ counter/serializers.py
@@ -3,8 +3,6 @@ from rest_framework import serializers
 from .models import JarCount
 
 class JarCountSerializer(serializers.ModelSerializer):
-    inventory_name = serializers.CharField(source='inventory.product_name', read_only=True)
-
     class Meta:
         model = JarCount
         fields = ['id', 'count', 'shift', 'timestamp']
