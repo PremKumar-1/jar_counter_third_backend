912e4ea7cb7ef995c6cd5757b72ac3de22c5a474
Prem Kumar <premkumar@Prems-MacBook-Air.local>
2024-07-22 18:24:49 -0500

    urls

--- counter/urls.py
+++ counter/urls.py
@@ -3,9 +3,9 @@ from rest_framework.routers import DefaultRouter
 from .views import JarCountViewSet, update_jar_count
 
 router = DefaultRouter()
-router.register(r'boxercounts', JarCountViewSet)
+router.register(r'jarcounts', JarCountViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('update_jar_count/', update_jar_count, name='update_jar_count'),
+    path('new_update_jar_count/', update_jar_count, name='update_jar_count'),
 ]
--- jar_counter/urls.py
+++ jar_counter/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('counter.urls')),
+    path('service/', include('counter.urls')),
 ]
 
